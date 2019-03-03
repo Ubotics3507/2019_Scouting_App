8ff3abf37563a01c8f2b7e86da471bcefb1859cc
JosephT8ertot <josepht8ertot@gmail.com>
2019-03-03 17:34:23 -0600

    This adds every team at OKC to the pit data and adds a text data saving function.

--- 2019_Scouting_App/app/src/main/java/com/applications/josep/a2019_scouting_app/PitDataActivity.java
+++ 2019_Scouting_App/app/src/main/java/com/applications/josep/a2019_scouting_app/PitDataActivity.java
@@ -1,51 +1,105 @@
 package com.applications.josep.a2019_scouting_app;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class PitDataActivity extends AppCompatActivity {
+
+    String Text_File = "data.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pit_data);
 
-        Spinner teamNum = (Spinner) findViewById(R.id.TeamNumSpinner);
+        final Spinner teamNum = (Spinner) findViewById(R.id.TeamNumSpinner);
         ArrayAdapter<String> teamNumAd = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.team_numbers));
         teamNumAd.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         teamNum.setAdapter(teamNumAd);
 
-        Spinner scoring = (Spinner) findViewById(R.id.BallHatchBothSpinner);
+        final Spinner scoring = (Spinner) findViewById(R.id.BallHatchBothSpinner);
         ArrayAdapter<String> scoringAd = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.ball_hatch_both));
         scoringAd.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         scoring.setAdapter(scoringAd);
 
-        Spinner sandstorm = (Spinner) findViewById(R.id.AutoCameraBothSpinner);
+        final Spinner sandstorm = (Spinner) findViewById(R.id.AutoCameraBothSpinner);
         ArrayAdapter<String> sandstormAd = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.auto_camera_both));
         sandstormAd.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sandstorm.setAdapter(sandstormAd);
 
-        Spinner climb = (Spinner) findViewById(R.id.ClimbSpinner);
+        final Spinner climb = (Spinner) findViewById(R.id.ClimbSpinner);
         ArrayAdapter<String> climbAd = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.climb));
         climbAd.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         climb.setAdapter(climbAd);
 
-        Spinner rocket = (Spinner) findViewById(R.id.RocketSpinner);
+        final Spinner rocket = (Spinner) findViewById(R.id.RocketSpinner);
         ArrayAdapter<String> rocketAd = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.rocket_reach));
         rocketAd.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         rocket.setAdapter(rocketAd);
 
-        Spinner drivetrain = (Spinner) findViewById(R.id.DriveTrainSpinner);
+        final Spinner drivetrain = (Spinner) findViewById(R.id.DriveTrainSpinner);
         ArrayAdapter<String> drivetrainAd = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.drive_train));
         drivetrainAd.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         drivetrain.setAdapter(drivetrainAd);
+
+        final Button addDataBtn = (Button)findViewById(R.id.AddData);
+        addDataBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sTeamNum = teamNum.getSelectedItem().toString();
+                String sScoring = scoring.getSelectedItem().toString();
+                String sSandstorm = sandstorm.getSelectedItem().toString();
+                String sClimb = climb.getSelectedItem().toString();
+                String sRocket = rocket.getSelectedItem().toString();
+                String sDrivetrain = drivetrain.getSelectedItem().toString();
+                String sBody = sTeamNum + " " + sScoring + " " + sSandstorm + " " + sClimb + " " +
+                        sRocket + " " + sDrivetrain + "\n";
+                save(sBody);
+            }
+        });
+    }
+
+    private void save(String text){
+        FileOutputStream fos = null;
+
+        try {
+            fos = openFileOutput(Text_File,MODE_APPEND);
+            fos.write(text.getBytes());
+            Toast.makeText(this,"Saved at " + getFilesDir() + "/" + Text_File,Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(fos!=null){
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
